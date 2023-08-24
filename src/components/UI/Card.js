d3545f9bcd555ca53d1c1125e08231ebbc43d43e
kzychl <khantzaya@cityholdings.com.mm>
2023-08-24 11:24:06 +0630

    Change Padding

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,5 +1,5 @@
 const Card=({children})=>{
-    let cardClass = "w-full shadow-xl max-w-md p-4 bg-purple border border-none rounded-lg sm:p-6 md:p-8 mx-auto mt-20";
+    let cardClass = "w-full shadow-xl max-w-md p-4 bg-purple border border-none rounded-lg sm:p-5 md:p-8 mx-auto mt-20";
     return (
         <div className={cardClass}>
             {children}
